562f162c6567b2d8d7ab0ee61d67cf1413290947
Solonarv <nstamm@gmx.de>
2024-12-01 19:43:24 +0100

    solve day 1

--- haskell/solutions/Day01.hs
+++ haskell/solutions/Day01.hs
@@ -2,10 +2,39 @@ module Day01 where
 
 import AOC.Solution
 
-solution :: Solution () () ()
+import ParsingPrelude
+import Util
+
+import Control.Lens
+import Data.List
+import Data.Map (Map)
+import Data.Map.Strict qualified as Map
+
+solution :: Solution ([Int], [Int]) [Int] Int
 solution = Solution
-  { decodeInput = pure ()
-  , solveA = defSolver
+  { decodeInput = fmap (over both sort . unzip) $ ((,) <$> decimal <*> (noeol *> decimal)) `sepBy` eol
+  , solveA = Solver
+    { solve = Just . uncurry (zipWith (\x y -> abs (x-y)))
+    , display = show . sum
+    }
   , solveB = defSolver
-  , tests = []
+    { solve = Just . getSum . Map.foldMapWithKey (\k n -> Sum (k*n)) . uncurry countSimilarity
+    }
+  , tests =
+    [ unlines
+      [ "3   4"
+      , "4   3"
+      , "2   5"
+      , "1   3"
+      , "3   9"
+      , "3   3"
+      ] :=> [(PartA, "11"), (PartB, "31")]
+
+    ]
   }
+
+-- Note: problem statement is unclear on how to handle duplicates in left list.
+-- Assuming they are to be counted multiple times.
+countSimilarity :: [Int] -> [Int] -> Map Int Int
+countSimilarity needles hay = Map.intersectionWith (*) (freq needles) (freq hay)
+  where freq = getFreqs . toFreqMap
