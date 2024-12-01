c4623fc4bcf01ddab549f0ed2ba386b080a76b57
Solonarv <nstamm@gmx.de>
2024-12-01 19:21:37 +0100

    fix up harness to work properly again

---
+++ haskell/solutions/Day01.hs
@@ -0,0 +1,11 @@
+module Day01 where
+
+import AOC.Solution
+
+solution :: Solution () () ()
+solution = Solution
+  { decodeInput = pure ()
+  , solveA = defSolver
+  , solveB = defSolver
+  , tests = []
+  }
